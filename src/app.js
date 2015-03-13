7ad01ae34e59303a6dc19d0995aaaec4f2bebaf9
Curtis Schlak <realistschuckle@gmail.com>
2015-03-13 05:51:41 -0500

    Add express to the app.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 import 'babel-core/polyfill';
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
 
-import http from 'http';
+let app = express();
+let inProduction = process.env.NODE_ENV === 'production';
 
-var server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<html><head><title>Hi</title></head><body><h1>HI!</h1><h2>Or, not.</h2></body></html>');
+if (!inProduction) {
+  console.log('Serving public files from express at ' + __dirname);
+  app.use(express.static(path.join(__dirname, 'public')));
+}
+
+app.get('/session', (req, res) => {
+  res.send('<html><head><title>Hi</title><link rel="stylesheet" href="/styles/font-awesome-8d61e0f1.css"></head><body><h1>HI!</h1><i class="fa fa-mercury"></i></body></html>');
+})
+
+var server = app.listen(3000, () => {
+  let host = server.address().address;
+  let port = server.address().port;
+
+  console.log('MCM listening at http://%s:%s', host, port);
 });
-
-server.listen(3000);
