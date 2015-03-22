c1cc38b17c368f7eacce473f7d90f592cb8946e5
Curtis Schlak <realistschuckle@gmail.com>
2015-03-22 07:19:10 -0500

    Require a file name for the newsletter.
    Used to create a FileDirective that names the download attachment
    correctly.

--- src/models/newsletter.js
+++ src/models/newsletter.js
@@ -3,6 +3,7 @@ import stork from 'stork-odm';
 let newsletter = stork.deliver('newsletter', function () {
   this.number('month', { required: true, minimum: 0, maximum: 11 });
   this.number('year', { required: true, minimum: 2000 });
+  this.string('fileName', { required: true });
   this.string('path', { required: true });
   this.string('authorId', { required: true });
   this.timestamps();
