3c00d0ec91bffd63a2b4160de6b0622c6e5727ae
Curtis Schlak <realistschuckle@gmail.com>
2015-04-09 12:15:57 -0500

    Beef up 'gulp dev' with a forever-type runner.

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ var babel = require('gulp-babel');
 var del = require('del');
 var exec = require('child_process').exec;
 var fs = require('fs');
+var keepup = require('keepup');
+var server = null;
 
 gulp.task('clean', function (cb) {
   del('./build', function (err) {
@@ -19,6 +21,10 @@ gulp.task('es6', function () {
     .pipe(gulp.dest('./build'));
 });
 
+gulp.task('reload', [ 'es6' ], function () {
+  server.reload();
+});
+
 gulp.task('label', [ 'build' ], function (cb) {
   exec('cp -R ./build ./dist', function (e) {
     var now = new Date();
@@ -30,11 +36,30 @@ gulp.task('label', [ 'build' ], function (cb) {
   });
 });
 
+gulp.task('serve', [ 'es6' ], function () {
+  server = keepup('npm start')
+    .on('start', function (data) {
+      console.info('GULP: Server started');
+    })
+    .on('crash', function (data) {
+      console.error('GULP:', data.captured);
+    })
+    .on('reload', function () {
+      console.trace('GULP: Server reloaded');
+    })
+    .on('data', function (buf) {
+      process.stdout.write(buf);
+    })
+    .on('stderr', function (buf) {
+      process.stderr.write(buf);
+    });
+});
+
 gulp.task('watch', [ 'build' ], function () {
-  gulp.watch('./src/**/*.js', [ 'es6' ]);
+  gulp.watch('./src/**/*.js', [ 'es6', 'reload' ]);
 });
 
 gulp.task('build', [ 'es6' ]);
 gulp.task('default', [ 'build' ]);
-gulp.task('dev', [ 'build', 'watch' ]);
+gulp.task('dev', [ 'build', 'serve', 'watch' ]);
 gulp.task('dist', [ 'label' ]);
