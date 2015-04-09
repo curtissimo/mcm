7068055f06fa718a32e25ff2a48f905fbf399871
Curtis Schlak <realistschuckle@gmail.com>
2015-04-09 13:09:24 -0500

    Put email stuff into the menu.

--- src/presenters/menu/presenter.js
+++ src/presenters/menu/presenter.js
@@ -1,5 +1,13 @@
 let presenter = {
   get(ac) {
+    if (ac.member) {
+      ac.member.permissions.hasPermission = function () {
+        return this.canManagePermissions
+            || this.canManageSettings
+            || this.canManagePublicDocuments
+            || this.canManageRoadCaptains;
+      };
+    }
     let data = {
       name: ac.settings.name,
       member: ac.member
