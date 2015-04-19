ce0db27f75b4c536364cde072773f3ec983d5ade
Curtis Schlak <realistschuckle@gmail.com>
2015-04-18 23:45:14 -0500

    Remove the mailer runner from the gulpfile because it crashes libuv.

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,6 @@ var gulp = require('gulp');
 var del = require('del');
 var rename = require('gulp-rename');
 var babel = require('gulp-babel');
-var keepup = require('keepup');
-
-var mailerDaemon = null;
-
 process.env.MCM_RABBIT_URL = 'amqp://curtis:curtis@web-server';
 process.env.MCM_DB = 'http://couchdb:5984';
 process.env.MCM_MAIL_HOST = 'web-server';
@@ -30,29 +26,12 @@ gulp.task('build:es6-server', function () {
 
 gulp.task('build:es6', [ 'build:es6-server', 'build:es6-client' ]);
 
+gulp.task('build', [ 'build:es6' ]);
+
 gulp.task('clean', function (next) {
   del([ './build', './dist' ], function (e) {
     next(e);
   });
 });
 
-gulp.task('run:reload:mailer', [ 'build:es6-server' ], function (next) {
-  mailerDaemon.reload()
-    .once('start', next());
-});
-
-gulp.task('run:start:mailer', [ 'build:es6-server' ], function (next) {
-  mailerDaemon = keepup('npm run mailer-daemon')
-    .once('start', next)
-    .on('crash', function (data) {
-      console.error('Mailer crashed:', data.captured);
-    })
-    .on('stderr', function (buf) {
-      console.error('Mailer:', buf.toString());
-    })
-    .on('data', function (buf) {
-      console.log('Mailer:', buf.toString());
-    });
-});
-
-gulp.task('run', [ 'run:start:mailer' ]);
+gulp.task('dev', []);
